c9f22e72a81916465f879dda403b73470b85bc81
Richo Healey <richo@psych0tik.net>
2015-06-25 14:38:57 -0700

    Sketch out an initial implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate rand;
+
+use std::io::Read;
+use std::fs::File;
+use rand::Rng;
+
+const DICT_FILE: &'static str = "/usr/share/dict/words";
+
 fn main() {
-    println!("Hello, world!");
+    let mut dict = File::open(DICT_FILE).unwrap();
+    // Not ideal, but whatever.
+    let mut buf = String::new();
+    dict.read_to_string(&mut buf);
+
+    let mut lines: Vec<_> = buf
+        .lines()
+        .filter(|x| x.len() > 3 && x.len() < 7)
+        .collect();
+    rand::thread_rng().shuffle(&mut lines);
+    for i in (0..4) {
+        print!("{} ", lines[i]);
+    }
 }
