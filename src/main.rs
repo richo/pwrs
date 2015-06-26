9b9ee42f1d00f59eb0e8e968b0fc9d4444beed02
Richo Healey <richo@psych0tik.net>
2015-06-26 09:34:36 -0700

    Option parsing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate rand;
+extern crate getopts;
 
 use std::io::{self, Read};
 use std::fs::File;
 use rand::Rng;
+use getopts::Options;
 
 const DEFAULT_DICT_FILE: &'static str = "/usr/share/dict/words";
 
@@ -46,8 +48,67 @@ fn shuffled_words<'a>(source: &'a String, cfg: &Config) -> Vec<&'a str> {
     lines
 }
 
+fn usage(opts: &Options, err: Option<String>) {
+    if let Some(e) = err {
+        println!("{}\n", e);
+    }
+    let brief = "Usage: pwrs [options]";
+    println!("{}", opts.usage(&brief));
+}
+
+
+fn config() -> Option<Config> {
+    let mut cfg = Config::new();
+    let args: Vec<_> = std::env::args().collect();
+    let mut opts = Options::new();
+    opts.optflag("h", "help", "Print this help");
+    opts.optopt("m", "min", "Minimum number of characters in words", "MIN");
+    opts.optopt("M", "max", "Maximum number of characters in words", "MAX");
+    opts.optopt("c", "count", "Count of passphrases to print", "COUNT");
+    opts.optopt("n", "number", "Number of words to include in passphrases", "NUMBER");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => {
+            usage(&opts, Some(f.to_string()));
+            // This is only in nightlies and for once I'm writing a thing that works on stable rust
+            // ::std::env::set_exit_status(1);
+            return None;
+        }
+    };
+
+    if matches.opt_present("help") {
+        usage(&opts, None);
+        return None;
+    }
+
+    macro_rules! unpack_opt(
+        ($name:ident) => {
+            if let Some(s) = matches.opt_str(stringify!($name)) {
+                match s.parse() {
+                    Ok(i) => cfg.$name = i,
+                    Err(e) => {
+                        usage(&opts, Some(e.to_string()));
+                        return None;
+                    }
+                }
+            }
+        }
+    );
+
+    unpack_opt!(min);
+    unpack_opt!(max);
+    unpack_opt!(count);
+    unpack_opt!(number);
+
+    Some(cfg)
+}
+
 fn main() {
-    let cfg = Config::new();
+    let cfg = match config() {
+        Some(c) => c,
+        None => return,
+    };
     let words = read_words(&cfg.filename).unwrap();
 
     let print = |word: &str| {
